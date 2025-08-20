bfecf556a6880098bae506a970f0318aa6688156
serhii__shulha <sagemini17@gmail.com>
2025-08-20 20:14:07 +0300

    removed console log

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -4,8 +4,6 @@ import path from 'path';
 const dbPath = path.join(process.cwd(), 'database.db');
 export const db = new Database(dbPath);
 
-console.log(dbPath)
-
 db.exec(`
   CREATE TABLE IF NOT EXISTS instructions (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
