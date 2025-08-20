6b3d1e87d1539f25ec9a76132eb2610b3ea2363c
serhii__shulha <sagemini17@gmail.com>
2025-08-20 18:45:24 +0300

    fixes

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,8 +1,10 @@
 import Database from 'better-sqlite3';
 import path from 'path';
 
-const dbPath = path.join(process.cwd(), 'src', 'db', 'database.db');
+const dbPath = path.join(process.cwd(), 'database.db');
 export const db = new Database(dbPath);
+
+console.log(dbPath)
 
 db.exec(`
   CREATE TABLE IF NOT EXISTS instructions (
